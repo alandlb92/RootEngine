61bf33cb38ba9d9bdc0ffab9b2e885b55548781a
Alan Bittencourt <alan.dlucas.bittencourt@gmail.com>
2024-07-26 14:09:20 -0300

    Matrix multiply

--- BitEngine/Include/Data/RMatrix4x4.h
+++ BitEngine/Include/Data/RMatrix4x4.h
@@ -10,6 +10,43 @@ struct RMatrix4x4
     float m20, m21, m22, m23;
     float m30, m31, m32, m33;
 
+    RMatrix4x4()
+    {
+        *this = RMatrix4x4::Identity();
+    };
+
+    RMatrix4x4(float _m00, float _m01, float _m02, float _m03,
+               float _m10, float _m11, float _m12, float _m13,
+               float _m20, float _m21, float _m22, float _m23,
+               float _m30, float _m31, float _m32, float _m33) :
+               m00(_m00), m01(_m01), m02(_m02), m03(_m03),
+               m10(_m10), m11(_m11), m12(_m12), m13(_m13),
+               m20(_m20), m21(_m21), m22(_m22), m23(_m23),
+               m30(_m30), m31(_m31), m32(_m32), m33(_m33)
+    {
+
+    };
+
+    RMatrix4x4 operator* (const RMatrix4x4& value)
+    {
+        return RMatrix4x4 (m00 * value.m00 + m01 * value.m10 + m02 * value.m20 + m03 * value.m30,  
+                           m00 * value.m01 + m01 * value.m11 + m02 * value.m21 + m03 * value.m31, 
+                           m00 * value.m02 + m01 * value.m12 + m02 * value.m22 + m03 * value.m32, 
+                           m00 * value.m03 + m01 * value.m13 + m02 * value.m23 + m03 * value.m33,        
+                           m10* value.m00 + m11 * value.m10 + m12 * value.m20 + m13 * value.m30,
+                           m10* value.m01 + m11 * value.m11 + m12 * value.m21 + m13 * value.m31,
+                           m10* value.m02 + m11 * value.m12 + m12 * value.m22 + m13 * value.m32,
+                           m10* value.m03 + m11 * value.m13 + m12 * value.m23 + m13 * value.m33,
+                           m20* value.m00 + m21 * value.m10 + m22 * value.m20 + m23 * value.m30,
+                           m20* value.m01 + m21 * value.m11 + m22 * value.m21 + m23 * value.m31,
+                           m20* value.m02 + m21 * value.m12 + m22 * value.m22 + m23 * value.m32,
+                           m20* value.m03 + m21 * value.m13 + m22 * value.m23 + m23 * value.m33,
+                           m30* value.m00 + m31 * value.m10 + m32 * value.m20 + m33 * value.m30,
+                           m30* value.m01 + m31 * value.m11 + m32 * value.m21 + m33 * value.m31,
+                           m30* value.m02 + m31 * value.m12 + m32 * value.m22 + m33 * value.m32,
+                           m30* value.m03 + m31 * value.m13 + m32 * value.m23 + m33 * value.m33);
+    }
+
     static RMatrix4x4 Identity()
     {
         RMatrix4x4 m;
