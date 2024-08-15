d16ee40237834d681b3664620a67f3681e02fc2a
Alan Bittencourt <alan.dlucas.bittencourt@gmail.com>
2024-08-15 18:15:59 -0300

    Refactoring the way to read textures [wip]

--- RootEngine/Source/Graphics/Texture2D.cpp
+++ RootEngine/Source/Graphics/Texture2D.cpp
@@ -3,7 +3,8 @@
 #include "Faia/Converter.h"
 #include <string>
 #include "Faia/Paths.h"
-#include "DirectXTex.h"
+//todo: remove directXText to more mult platform way
+//#include "DirectXTex.h"
 
 
 using namespace Faia::Paths;
@@ -20,20 +21,59 @@ namespace Faia
             std::wstring texturePath(GetApplicationFolderPath());
             texturePath.append(Faia::Converter::CharToLPCWSTR(textureRelativePath));
 
-            ScratchImage image;
-            HRESULT hr = LoadFromWICFile(texturePath.c_str(), WIC_FLAGS_NONE, nullptr, image);
-
-            if (FAILED(hr))
+            //Texture description
+            D3D11_TEXTURE2D_DESC texDesc;
+            ZeroMemory(&texDesc, sizeof(D3D11_TEXTURE2D_DESC));
+            texDesc.Width = 5;
+            texDesc.Height = 5;
+            texDesc.MipLevels = 1;
+            texDesc.ArraySize = 1;
+            texDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+            texDesc.SampleDesc.Count = 1;
+            texDesc.SampleDesc.Quality = 0;
+            texDesc.Usage = D3D11_USAGE_DEFAULT;
+            texDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+            texDesc.CPUAccessFlags = 0;
+            texDesc.MiscFlags =  0;
+            //Todo: read the texture from a file we will do a importer for the texture.
+            struct rgbaTest
             {
-                std::string msg;
-                msg.append("Fail to load image path: ");
-                msg.append((const char*)texturePath.c_str());
-                OutputDebugStringA(msg.c_str());
-                throw std::invalid_argument(msg);
-            }
+                uint8_t r;
+                uint8_t g;
+                uint8_t b;
+                uint8_t a;
+            };
 
-            hr = CreateShaderResourceView(device, image.GetImages(), image.GetImageCount(), image.GetMetadata(), _textureSRV.GetAddressOf());
 
+            rgbaTest tex[25] = {rgbaTest{255, 0, 0, 255}, rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},
+                                rgbaTest{255, 0, 0, 255}, rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},
+                                rgbaTest{255, 0, 0, 255}, rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},
+                                rgbaTest{255, 0, 0, 255}, rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},
+                                rgbaTest{255, 0, 0, 255}, rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255},rgbaTest{255, 0, 0, 255}
+                                };
+
+            //Texture data
+            D3D11_SUBRESOURCE_DATA texData;
+            ZeroMemory(&texData, sizeof(D3D11_SUBRESOURCE_DATA));
+            texData.pSysMem = tex;
+            texData.SysMemPitch = texDesc.Width * sizeof(rgbaTest);
+            texData.SysMemSlicePitch = 0;
+
+            //ScratchImage image;
+            ID3D11Texture2D* texture = nullptr;
+            HRESULT hr = device->CreateTexture2D(&texDesc, &texData, &texture);
+
+            D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
+            ZeroMemory(&srvDesc, sizeof(D3D11_SHADER_RESOURCE_VIEW_DESC));
+            srvDesc.Format = texDesc.Format;
+            srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+            srvDesc.Texture2D.MostDetailedMip = 0;
+            srvDesc.Texture2D.MipLevels = -1;
+
+
+            hr = device->CreateShaderResourceView(texture, &srvDesc, _textureSRV.GetAddressOf());
+
+            //Sampler
 
             D3D11_SAMPLER_DESC samplerDesc;
             ZeroMemory(&samplerDesc, sizeof(samplerDesc));
